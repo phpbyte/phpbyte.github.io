8b0ab6816885965c1edb07a6a3f9dbd7616583f6
phpbyte <138843700+phpbyte@users.noreply.github.com>
2023-07-07 14:25:48 +0700

    Update style.js
    change recommanded

--- js/style.js
+++ js/style.js
@@ -1,15 +1,28 @@
-async function postData(url = "", data = {}) {
-   const result = await fetch(url, {
-    method: "POST", 
-    mode: "cors",
-    cache: "no-cache", 
-    headers: {
-      "Content-Type": "application/json"
-    },
-    redirect: "follow",
-    referrerPolicy: "no-referrer",
-    body: JSON.stringify(data)
-  }); return result.json()
+async function postData(url, data={}) {
+  //  const result = await fetch(url, {
+  //   method: "POST", 
+  //   mode: "cors",
+  //   cache: "no-cache", 
+  //   headers: {
+  //     "Content-Type": "application/json"
+  //   },
+  //   redirect: "follow",
+  //   referrerPolicy: "no-referrer",
+  //   body: JSON.stringify(data)
+  // }); return result.json()
+   const parsHeader = new Headers({
+            "Content-Type": "application/x-www-form-urlencoded"
+         }),
+         urlEncoded = new URLSearchParams(data),
+         optionData = {
+            method: 'POST',
+            headers: parsHeader,
+            mode: "cors",
+            cache: "no-cache", 
+            body: urlEncoded,
+         };
+   return await fetch(url, optionData)
+
 }
 postData("https://riza.us/ask.php",{answer:7}).then((data) => {
   console.log(data);
